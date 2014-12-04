57c9dbd1c09b31ff21582458fa0069dec386b41e
Zoinz <danicool118@gmail.com>
2014-12-04 11:00:41 +0100

    Opening Windows

--- SKPL.UserInterface/MainWindow.xaml.cs
+++ SKPL.UserInterface/MainWindow.xaml.cs
@@ -30,5 +30,17 @@ namespace SKPL.UserInterface
             var add = new AddData();
             add.Show();
         }
+
+        private void Redigere_Click(object sender, RoutedEventArgs e)
+        {
+            var edit = new EditData();
+            edit.Show();
+        }
+
+        private void Slet_Click(object sender, RoutedEventArgs e)
+        {
+            var delete = new DeleteData();
+            delete.Show();
+        }
     }
 }
