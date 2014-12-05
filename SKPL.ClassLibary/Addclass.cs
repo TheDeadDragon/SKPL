dd3cd7fd29f343d33fc1881cc802f81e24c1579f
TheDeadDragon <mjet@live.dk>
2014-12-05 10:59:11 +0100

    changes

---
+++ SKPL.ClassLibary/Addclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKPL.ClassLibary
+{
+    class Addclass
+    {
+        private string _addCBItem;
+
+        public string AddCBItem
+        {
+            get { return _addCBItem; }
+            set
+            {
+                switch (_addCBItem)
+                {
+                    case TeacherBoxItem:
+
+                        break;
+                    case StudentBoxItem:
+
+                        break;
+                }
+            }
+        }
+    }
+}
--- SKPL.UserInterface/Views/AddData.xaml.cs
+++ SKPL.UserInterface/Views/AddData.xaml.cs
@@ -14,49 +14,5 @@ namespace SKPL.UserInterface.Views
         {
             InitializeComponent();
         }
-
-        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            int AddBoxvalue = AddBox.SelectedIndex;
-            switch (AddBoxvalue)
-            {
-                case 0:
-                    TeacherBox.Visibility = Visibility.Visible;
-                    StudentBox.Visibility = Visibility.Hidden;
-                    ClassBox.Visibility = Visibility.Hidden;
-                    LectureBox.Visibility = Visibility.Hidden;
-                    break;
-                case 1:
-                    TeacherBox.Visibility = Visibility.Hidden;
-                    StudentBox.Visibility = Visibility.Hidden;
-                    ClassBox.Visibility = Visibility.Hidden;
-                    LectureBox.Visibility = Visibility.Visible;
-                    break;
-                case 2:
-                    TeacherBox.Visibility = Visibility.Hidden;
-                    StudentBox.Visibility = Visibility.Hidden;
-                    ClassBox.Visibility = Visibility.Visible;
-                    LectureBox.Visibility = Visibility.Hidden;
-                    break;
-                case 3:
-                    TeacherBox.Visibility = Visibility.Hidden;
-                    StudentBox.Visibility = Visibility.Visible;
-                    ClassBox.Visibility = Visibility.Hidden;
-                    LectureBox.Visibility = Visibility.Hidden;
-                    break;
-                case 4:
-                    TeacherBox.Visibility = Visibility.Hidden;
-                    StudentBox.Visibility = Visibility.Hidden;
-                    ClassBox.Visibility = Visibility.Hidden;
-                    LectureBox.Visibility = Visibility.Hidden;
-                    break;
-                default:
-                    TeacherBox.Visibility = Visibility.Visible;
-                    StudentBox.Visibility = Visibility.Hidden;
-                    ClassBox.Visibility = Visibility.Hidden;
-                    LectureBox.Visibility = Visibility.Hidden;
-                    break;
-            }
-        }
     }
 }
