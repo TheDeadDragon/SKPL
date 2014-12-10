e75489debdb2d2a5527adb8a58c9310df807a137
TheDeadDragon <mjet@live.dk>
2014-12-10 08:53:28 +0100

    Resync some stuff

--- SKPL.UserInterface/ViewModels/ViewModelClass.cs
+++ SKPL.UserInterface/ViewModels/ViewModelClass.cs
@@ -9,7 +9,6 @@ using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace SKPL.UserInterface.ViewModels
 {
